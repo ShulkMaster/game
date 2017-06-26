0650ff2693d3b8a2637e5952ef2a1aef96b063ea
ShulkMaster <yury_081297@outlook.com>
2017-06-26 01:14:13 -0600

    adds a new crappy fire efect

--- src/StateMachine/Menu.java
+++ src/StateMachine/Menu.java
@@ -16,6 +16,8 @@ public class Menu implements GameState {
         private final XBoton botonscore;
         private final XBoton botonsalir;
         private final JLabel menuicon;
+        private final JLabel fuego;
+        private final JLabel fuego2;
         private final Font fuentex = new java.awt.Font("Resources/fonts/fontana/fuente.ttf", 2, 15);
         private final ButtonListener lbutton = new ButtonListener();
 	
@@ -27,14 +29,14 @@ public class Menu implements GameState {
         botonstart.setBackground(Color.CYAN);
         botonstart.setBounds(401,350,130,30); 
         botonstart.setFont(fuentex);
-        botonstart.setText("LordZerxes");
+        botonstart.setText("Play");
         botonstart.addActionListener(lbutton);
         //boton de scorea
         botonscore = new XBoton(Color.BLUE, Color.CYAN);
         botonscore.setBackground(Color.CYAN);
         botonscore.setBounds(401,420,130,30);
         botonscore.setFont(fuentex);
-        botonscore.setText("Escores");
+        botonscore.setText("puntajes");
         botonscore.addActionListener(lbutton);
         //boton salir 
         botonsalir = new XBoton(Color.BLUE, Color.CYAN);
@@ -43,10 +45,18 @@ public class Menu implements GameState {
         botonsalir.setFont(fuentex);
         botonsalir.setText("Salir");
         botonsalir.addActionListener(lbutton);
-        //icono del menu XD
+        //icono del menu XD las calaveras we
         menuicon = new JLabel();
         menuicon.setIcon(getIconImage("/Resources/image/calavera.png"));
         menuicon.setBounds(66, 0,800 , 328);
+        // dibuja el fueguito :3
+        fuego = new JLabel();
+        fuego.setIcon(getIconImage("/Resources/image/fire.gif"));
+        fuego.setBounds(0, 358,400 , 300);
+        fuego2 = new JLabel();
+        fuego2.setIcon(getIconImage("/Resources/image/fire.gif"));
+        fuego2.setBounds(400, 358,400 , 300);
+        
 	JPanel menuPanel = CurrentData.menuPanel;
         menuPanel.setBackground(Color.BLACK);
         menuPanel.setLayout(null);
@@ -55,6 +65,8 @@ public class Menu implements GameState {
         menuPanel.add(botonscore);
         menuPanel.add(botonsalir);
         menuPanel.add(menuicon);
+        menuPanel.add(fuego);
+        menuPanel.add(fuego2);
         CurrentData.layout.show( CurrentData.panel, CurrentData.menu);
         CurrentData.frame.revalidate();
         
