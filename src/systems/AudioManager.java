ce14aa83f6dda5cdb61aa6279a27fc13806ec402
Arturo <arturo7xz@gmail.com>
2017-07-05 03:10:39 -0600

    on going battle system work

--- src/systems/AudioManager.java
+++ src/systems/AudioManager.java
@@ -6,45 +6,79 @@ import java.io.*;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
 
 public class AudioManager {
 	
 	//private static AudioStream music;
+    private static AudioPlayer MusicPlayer = AudioPlayer.player;
+	private static AudioStream BGM;
 	private static AudioStream sound;
-	private static Thread MusicPlayer;
-	private static ContinuousAudioDataStream loopyLoopytyLoop;
-	//private static Clip clip;
-	
-    public static void playMusic( final String path){
-		MusicPlayer = new Thread(){
+    private static AudioData data;
+    private static ContinuousAudioDataStream loop;
+
+	private static Thread MusicThread;
+	private static Clip clip;
+	private static Clip effect;
+	private static AudioInputStream inputStream;
+	private static AudioInputStream fx;
+
+
+    public static void loadSong( final String path){
+        //AudioStream music = null; //ruta incluye src/
+        /*try {
+            BGM = new AudioStream(new FileInputStream(path));
+            data = BGM.getData();
+            loop = new ContinuousAudioDataStream( data );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+        try {
+            clip = AudioSystem.getClip();
+            inputStream = AudioSystem.getAudioInputStream( AudioManager.class.getResourceAsStream(path));
+            clip.open(inputStream);
+        } catch ( Exception e) {
+            e.printStackTrace();
+        }
+                // usa ruta que no incluye src/
+    }
+
+    public static void loadEffect( final String path){
+        try {
+            effect = AudioSystem.getClip();
+            fx = AudioSystem.getAudioInputStream( AudioManager.class.getResourceAsStream(path));
+            effect.open(fx);
+        } catch ( Exception e) {
+            e.printStackTrace();
+        }
+        // usa ruta que no incluye src/
+    }
+
+    public static void playMusic(){
+		MusicThread = new Thread(){
 		    @Override public void run() {
                 try {
-                    AudioStream music = new AudioStream(new FileInputStream(path)); //ruta incluye src/
-                    AudioData data = music.getData();
-                    loopyLoopytyLoop = new ContinuousAudioDataStream( data);
-                    AudioPlayer.player.start(loopyLoopytyLoop);
-                    /*clip = AudioSystem.getClip();
-                    AudioInputStream inputStream = AudioSystem.getAudioInputStream(
-                            AudioManager.class.getResourceAsStream(path)); // usa ruta que no incluye src/
-                    clip.open(inputStream);
-                    clip.start();*/
+                    //MusicPlayer.player.start( loop );
+                    clip.start();
+                    clip.loop(10);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         };
-		MusicPlayer.start();
+		MusicThread.start();
 	}
 
 	public static void stopMusic(){
-        //clip.stop();
-        AudioPlayer.player.stop( loopyLoopytyLoop);
+        clip.stop();
+        //MusicPlayer.player.stop( loop );
 
     }
-	public static void playSound( String path ){
+	public static void playSound( ){
         try{
-	        sound = new AudioStream(new FileInputStream(path));
-	        AudioPlayer.player.start(sound);
+            effect.start();
+	        //sound = new AudioStream(new FileInputStream(path));
+	        //AudioPlayer.player.start(sound);
         }catch(Exception e){
             e.printStackTrace();
         }
